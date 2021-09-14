ef6277a1dcf4aa0f3673e9605c6fe4fc7e6ef59b
ramesh45345 <6318224+ramesh45345@users.noreply.github.com>
2021-09-14 09:12:26 -0400

    Add mime set script

---
+++ CMimeSet.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+"""Set Mime Types"""
+
+# Python includes.
+import argparse
+import glob
+import os
+import shutil
+import sys
+import subprocess
+
+# Folder of this script
+SCRIPTDIR = sys.path[0]
+
+# Global variables
+types_archive = "application/x-7z-compressed,application/x-xz-compressed-tar,application/zip,application/x-compressed-tar,application/x-bzip-compressed-tar,application/x-tar,application/x-xz"
+types_text = "text/plain"
+
+### Functions ###
+def Mime_CheckCmds():
+    """Check for required utilities."""
+    cmdcheck = ["xdg-mime"]
+    for cmd in cmdcheck:
+        if not shutil.which(cmd):
+            sys.exit("\nError, ensure command {0} is installed.".format(cmd))
+def Mime_Set(mimetype: str, app: str):
+    """Set mime-type."""
+    subprocess.run(["xdg-mime", "default", app, mimetype], check=False)
+def Mime_Query(mimetype: str):
+    """Query currently set mime-type."""
+    output = subprocess.run(["xdg-mime", "query", "default", mimetype], stdout=subprocess.PIPE, universal_newlines=True, check=False).stdout.strip()
+    print("Mime: {0}\tSetting: {1}".format(mimetype, output))
+def Mime_Set_All(mimes: str, app: str):
+    """
+    Set all mime-types.
+    mimes should be comma delimited.
+    """
+    if FindDesktopFile(app) is False:
+        print("ERROR: desktop file {0} not found.".format(app))
+    split_mimes = mimes.split(",")
+    for mime in split_mimes:
+        Mime_Set(mime, app)
+def Mime_Query_All(mimes):
+    """
+    Query all mime-types.
+    mimes should be comma delimited.
+    """
+    split_mimes = mimes.split(",")
+    for mime in split_mimes:
+        Mime_Query(mime)
+def FindDesktopFile(desktop_ref: str):
+    """Find out if a desktop file exists."""
+    desktopref_exists = False
+    XDG_DATA_DIRS = os.environ.get('XDG_DATA_DIRS').split(":")
+    for xdg_folder in XDG_DATA_DIRS:
+        xdg_app_folder = os.path.join(xdg_folder, "applications")
+        if os.path.isdir(xdg_app_folder):
+            if glob.glob(os.path.join(xdg_app_folder, desktop_ref)):
+                desktopref_exists = True
+    return desktopref_exists
+
+
+if __name__ == '__main__':
+    print("Running {0}".format(__file__))
+
+    # Get arguments
+    parser = argparse.ArgumentParser(description='Enter a chroot.')
+    parser.add_argument("-s", "--set", help='Set mimetypes. (i.e. application/"x-7z-compressed,application/x-xz-compressed-tar". Must set app with this option.')
+    parser.add_argument("-a", "--application", help='Application to set mimetype to (i.e. "org.kde.ark.desktop". Must be used with set options.')
+    parser.add_argument("-q", "--query", help='Query mimetypes. (i.e. "application/x-7z-compressed,application/x-xz-compressed-tar")')
+    parser.add_argument("-v", "--archives", help='Query archives. Set when combined with -a flag.', action="store_true")
+    parser.add_argument("-w", "--text", help='Query texts. Set when combined with -a flag.', action="store_true")
+    args = parser.parse_args()
+
+    # Ensure proper commands are on system.
+    Mime_CheckCmds()
+
+    # Query command
+    if args.query:
+        Mime_Query_All(args.query)
+    # Passed set command
+    elif args.set:
+        Mime_Set_All(args.set, args.application)
+        # Confirm settings
+        Mime_Query_All(args.set)
+    elif args.archives:
+        if args.application:
+            Mime_Set_All(types_archive, args.application)
+        # Query all archive types
+        Mime_Query_All(types_archive)
+    elif args.text:
+        if args.application:
+            Mime_Set_All(types_text, args.application)
+        # Query all archive types
+        Mime_Query_All(types_text)
