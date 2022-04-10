19176a1ee7ba9e25abf8506e23516ffd92001efd
ramesh45345 <6318224+ramesh45345@users.noreply.github.com>
2022-04-10 14:47:34 -0400

    Add pip

--- MFedoraSilverblue.py
+++ MFedoraSilverblue.py
@@ -111,7 +111,7 @@ if args.stage == 1:
 
     ### OSTree Apps ###
     # Cli tools
-    rostreeinstall("fish zsh tmux iotop p7zip util-linux-user fuse-sshfs redhat-lsb-core powerline-fonts google-roboto-fonts samba hdparm cups-pdf syncthing numix-icon-theme numix-icon-theme-circle tigervnc xrandr xset")
+    rostreeinstall("fish zsh tmux iotop p7zip util-linux-user fuse-sshfs redhat-lsb-core powerline-fonts google-roboto-fonts samba hdparm cups-pdf syncthing numix-icon-theme numix-icon-theme-circle tigervnc xrandr xset python3-pip")
     subprocess.run("systemctl enable sshd", shell=True, check=True)
     # NTP Configuration
     subprocess.run("systemctl enable systemd-timesyncd; timedatectl set-local-rtc false; timedatectl set-ntp 1", shell=True, check=True)
