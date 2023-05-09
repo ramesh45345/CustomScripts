347702e784104a9876637c1d70cc06a8399902f6
ramesh45345 <6318224+ramesh45345@users.noreply.github.com>
2023-05-09 19:58:16 -0400

    Add nala to sudoers for Ubuntu

--- MUbuntu.py
+++ MUbuntu.py
@@ -144,6 +144,7 @@ if __name__ == '__main__':
     CFunc.AddLineToSudoersFile(sudoersfile, "{0} ALL=(ALL) NOPASSWD: {1}".format(USERNAMEVAR, shutil.which("apt")))
     CFunc.AddLineToSudoersFile(sudoersfile, "{0} ALL=(ALL) NOPASSWD: {1}".format(USERNAMEVAR, shutil.which("apt-get")))
     CFunc.AddLineToSudoersFile(sudoersfile, "{0} ALL=(ALL) NOPASSWD: {1}".format(USERNAMEVAR, shutil.which("snap")))
+    CFunc.AddLineToSudoersFile(sudoersfile, "{0} ALL=(ALL) NOPASSWD: {1}".format(USERNAMEVAR, shutil.which("nala")))
 
     # Network Manager
     CFunc.aptinstall("network-manager network-manager-ssh")
