d7204278bf7ba89969bba5c394953f8e2dd5a28f
ramesh45345 <6318224+ramesh45345@users.noreply.github.com>
2024-09-15 20:12:55 -0400

    Add btrfs subvol creation

--- ZSlimDrive.py
+++ ZSlimDrive.py
@@ -3,6 +3,7 @@
 
 # Python includes.
 import argparse
+import functools
 import os
 import sys
 import subprocess
@@ -11,20 +12,31 @@ import time
 # Custom includes
 import CFunc
 
+# Disable buffered stdout (to ensure prints are in order)
+print = functools.partial(print, flush=True)
+
 # Globals
 SCRIPTDIR = os.path.abspath(os.path.dirname(__file__))
 
 # Get arguments
 parser = argparse.ArgumentParser(description='Automatically create partition scheme for VMs.')
-parser.add_argument("-n", "--noprompt", help='Do not prompt to continue.', action="store_true")
+parser.add_argument("-b", "--btrfs", help="Format with btrfs, with default subvolumes.", action="store_true")
+parser.add_argument("-d", "--blockdevice", help='Block Device to use')
 parser.add_argument("-f", "--filesystem", help='Filesystem (i.e, ext4, btrfs, xfs...)', default="ext4")
-parser.add_argument("-b", "--blockdevice", help='Block Device to use')
 parser.add_argument("-g", "--gpt", help='Use GPT partitioning for EFI', action="store_true")
-parser.add_argument("-p", "--pathtomount", help='Path to mount partitions', default="/mnt")
 parser.add_argument("-k", "--keep", help='Keep existing partitions (do not format)', action="store_true")
+parser.add_argument("-n", "--noprompt", help='Do not prompt to continue.', action="store_true")
+parser.add_argument("-p", "--pathtomount", help='Path to mount partitions', default="/mnt")
 
 # Save arguments.
 args = parser.parse_args()
+
+# Exit if not root.
+CFunc.is_root(True)
+
+# Process arguments.
+if args.btrfs:
+    args.filesystem = "btrfs"
 print("Filesystem:", args.filesystem)
 print("User-specified Block Device (if any):", args.blockdevice)
 if args.blockdevice is not None and os.path.exists(args.blockdevice) is True and stat.S_ISBLK(os.stat(args.blockdevice).st_mode) is True:
@@ -36,13 +48,10 @@ elif os.path.exists("/dev/vda") is True and stat.S_ISBLK(os.stat("/dev/vda").st_
 else:
     sys.exit("\nError, no block device detected. Please specify one.")
 print("Block Device to use:", devicetopartition)
-blocksize_string = subprocess.run('blockdev --getsize64 {0}'.format(devicetopartition), shell=True, check=True, stdout=subprocess.PIPE, universal_newlines=True)
+blocksize_string = subprocess.run(f'blockdev --getsize64 {devicetopartition}', shell=True, check=True, stdout=subprocess.PIPE, universal_newlines=True)
 blocksize = int(blocksize_string.stdout.strip())
 blocksizeMB = int(blocksize / 1000000)
-print("Size of Block Device: {0} MB".format(blocksizeMB))
-
-# Exit if not root.
-CFunc.is_root(True)
+print(f"Size of Block Device: {blocksizeMB} MB")
 
 if args.noprompt is False:
     input("Press Enter to continue.")
@@ -73,37 +82,50 @@ done
     subprocess.run(REMOVESCRIPT, shell=True, check=False)
 
     # Zero out first few mb of drive.
-    subprocess.run('dd if=/dev/zero of="{0}" bs=1M count=4 conv=notrunc'.format(devicetopartition), shell=True, check=True)
+    subprocess.run(f'dd if=/dev/zero of="{devicetopartition}" bs=1M count=4 conv=notrunc', shell=True, check=True)
     # Set up drive as gpt if true.
     if args.gpt is True:
-        subprocess.run('parted -s -a optimal "{0}" -- mktable gpt'.format(devicetopartition), shell=True, check=True)
+        subprocess.run(f'parted -s -a optimal "{devicetopartition}" -- mktable gpt', shell=True, check=True)
     else:
-        subprocess.run('parted -s -a optimal "{0}" -- mktable msdos'.format(devicetopartition), shell=True, check=True)
+        subprocess.run(f'parted -s -a optimal "{devicetopartition}" -- mktable msdos', shell=True, check=True)
 
     # Create partitions
     efisize = 100
     mainsize = blocksizeMB - efisize
     if args.gpt:
-        subprocess.run('parted -s -a optimal "{0}" -- mkpart primary ext2 1 {1}'.format(devicetopartition, mainsize), shell=True, check=True)
-        subprocess.run('parted -s -a optimal "{0}" -- mkpart primary fat32 {1} 100%'.format(devicetopartition, mainsize), shell=True, check=True)
+        subprocess.run(f'parted -s -a optimal "{devicetopartition}" -- mkpart primary ext2 1 {mainsize}', shell=True, check=True)
+        subprocess.run(f'parted -s -a optimal "{devicetopartition}" -- mkpart primary fat32 {mainsize} 100%', shell=True, check=True)
         # Set the efi partition to be bootable on gpt.
-        subprocess.run('parted -s -a optimal "{0}" -- set 2 boot on'.format(devicetopartition), shell=True, check=True)
+        subprocess.run(f'parted -s -a optimal "{devicetopartition}" -- set 2 boot on', shell=True, check=True)
         # Wait for partitions to exist
         os.sync()
         time.sleep(1)
         # Format EFI partition
-        subprocess.run('mkfs.vfat -n efi -F 32 {0}2'.format(devicetopartition), shell=True, check=True)
+        subprocess.run(f'mkfs.vfat -n efi -F 32 {devicetopartition}2', shell=True, check=True)
     else:
-        subprocess.run('parted -s -a optimal "{0}" -- mkpart primary ext2 1 100%'.format(devicetopartition), shell=True, check=True)
+        subprocess.run(f'parted -s -a optimal "{devicetopartition}" -- mkpart primary ext2 1 100%', shell=True, check=True)
         # Wait for partitions to exist
         os.sync()
         time.sleep(1)
     # Format main partition
-    subprocess.run('mkfs.{0} {1}1'.format(args.filesystem, devicetopartition), shell=True, check=True)
-    subprocess.run('fdisk -l {0}'.format(devicetopartition), shell=True, check=True)
+    subprocess.run(f'mkfs.{args.filesystem} {devicetopartition}1', shell=True, check=True)
+    if args.btrfs or args.filesystem == "btrfs":
+        subprocess.run(f'mount {devicetopartition}1 {args.pathtomount}', shell=True, check=True)
+        # Create subvolumes in mounted root
+        subprocess.run(f'btrfs subvolume create {args.pathtomount}/root', shell=True, check=True)
+        subprocess.run(f'btrfs subvolume create {args.pathtomount}/home', shell=True, check=True)
+        subprocess.run(f'btrfs subvolume create {args.pathtomount}/var', shell=True, check=True)
+        # Set default subvolume
+        subprocess.run(f'btrfs subvolume set-default {args.pathtomount}/root', shell=True, check=True)
+        # Unmount.
+        subprocess.run(f'umount -f {devicetopartition}1 ; umount -l {devicetopartition}1 ; sleep 1', shell=True, check=False)
+    subprocess.run(f'fdisk -l {devicetopartition}', shell=True, check=True)
 
 # Mount the parititons
-subprocess.run('mount {0}1 {1}'.format(devicetopartition, args.pathtomount), shell=True, check=True)
+subprocess.run(f'mount {devicetopartition}1 {args.pathtomount}', shell=True, check=True)
+if args.btrfs or args.filesystem == "btrfs":
+    subprocess.run(f'mkdir {args.pathtomount}/home ; mount -o subvol=/home,compress=zstd:1 {devicetopartition}1 {args.pathtomount}/home', shell=True, check=True)
+    subprocess.run(f'mkdir {args.pathtomount}/var ; mount -o subvol=/var,compress=zstd:1 {devicetopartition}1 {args.pathtomount}/var', shell=True, check=True)
 if args.gpt is True:
-    os.makedirs("{0}/boot/efi".format(args.pathtomount), exist_ok=True)
-    subprocess.run('mount {0}2 {1}/boot/efi'.format(devicetopartition, args.pathtomount), shell=True, check=True)
+    os.makedirs(f"{args.pathtomount}/boot/efi", exist_ok=True)
+    subprocess.run(f'mount {devicetopartition}2 {args.pathtomount}/boot/efi', shell=True, check=True)
