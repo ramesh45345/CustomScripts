88e3b4c8d79a15962f2149d108c498427bc993d6
ramesh45345 <6318224+ramesh45345@users.noreply.github.com>
2024-12-21 10:06:38 -0500

    Add almalinux

--- Mtbx.py
+++ Mtbx.py
@@ -17,7 +17,7 @@ print("Running {0}".format(__file__))
 SCRIPTDIR = os.path.abspath(os.path.dirname(__file__))
 
 # Global variables
-distro_options = ["all", "arch", "ubuntu", "fedora"]
+distro_options = ["all", "arch", "ubuntu", "fedora", "alma"]
 
 
 ### Functions ###
@@ -28,9 +28,12 @@ def check_cmds(cmdcheck: list):
             sys.exit("\nError, ensure command {0} is installed.".format(cmd))
 def addtext(ct_text: str, distro: str = "all"):
     """Add text to containerfiles."""
+    global containerfile_alma
     global containerfile_arch
     global containerfile_fedora
     global containerfile_ubuntu
+    if distro == "all" or distro == "alma":
+        containerfile_alma += "\n" + ct_text
     if distro == "all" or distro == "arch":
         containerfile_arch += "\n" + ct_text
     if distro == "all" or distro == "fedora":
@@ -58,6 +61,8 @@ print("Distro:", args.distro)
 
 ### Prune ###
 if args.prune:
+    if args.distro == "all" or args.distro == "alma":
+        subprocess.run("podman rmi --force alma-img", check=False, shell=True)
     if args.distro == "all" or args.distro == "arch":
         subprocess.run("podman rmi --force arch-img", check=False, shell=True)
     if args.distro == "all" or args.distro == "fedora":
@@ -67,6 +72,10 @@ if args.prune:
 
 
 ### Create containerfile ###
+containerfile_alma = """
+FROM quay.io/almalinuxorg/almalinux:latest
+ENV NAME=alma-img
+"""
 containerfile_arch = """
 FROM docker.io/library/archlinux:base-devel
 ENV NAME=arch-img
@@ -129,6 +138,19 @@ RUN dnf -y install make gcc automake autoconf ninja-build
 RUN dnf -y install wine winetricks
 """, "fedora")
 addtext(r"""
+# From stock image.
+RUN sed -i '/tsflags=nodocs/d' /etc/dnf/dnf.conf
+RUN dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm https://dl.fedoraproject.org/pub/epel/epel-next-release-latest-9.noarch.rpm
+RUN dnf install -y https://download1.rpmfusion.org/free/el/rpmfusion-free-release-9.noarch.rpm https://download1.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-9.noarch.rpm
+RUN dnf -y install --allowerasing acl bash nano curl gawk grep gzip libcap openssl p11-kit pam python3 rpm sed systemd tar
+RUN dnf -y install bash-completion bzip2 diffutils dnf-plugins-core findutils flatpak-spawn fuse fuse-libs git gnupg gnupg2-smime gvfs-client hostname iputils keyutils krb5-libs less lsof man-db man-pages mtr openssh-clients passwd pigz procps-ng rsync shadow-utils sudo tcpdump time tmux traceroute tree unzip vte-profile wget which words xorg-x11-xauth xz zip
+
+# Shell Tools
+RUN dnf -y install zsh
+# Build tools
+RUN dnf -y install make gcc automake autoconf
+""", "alma")
+addtext(r"""
 ARG DEBIAN_FRONTEND=noninteractive
 RUN apt-get update; apt-get -y upgrade
 RUN apt-get install -y sudo libcap2-bin bash zsh fish nano p7zip-full p7zip-rar unrar wget curl rsync less python-is-python3 python3-pip git lsb-release software-properties-common apt-transport-https gnupg tmux
@@ -166,17 +188,31 @@ addtext("RUN ln -sf /usr/share/zoneinfo/US/Eastern /etc/localtime")
 # Cleanup
 addtext("RUN pacman -Scc --noconfirm", "arch")
 addtext("RUN dnf clean all", "fedora")
+addtext("RUN dnf clean all", "alma")
 addtext("RUN apt-get clean", "ubuntu")
 # Command
 addtext('CMD ["zsh"]')
 
 # Write containerfiles
+tempfolder_alma = os.path.join(tempfile.gettempdir(), "ct_tb_alma")
 tempfolder_arch = os.path.join(tempfile.gettempdir(), "ct_tb_arch")
 tempfolder_fedora = os.path.join(tempfile.gettempdir(), "ct_tb_fedora")
 tempfolder_ubuntu = os.path.join(tempfile.gettempdir(), "ct_tb_ubuntu")
+tempfile_alma = os.path.join(tempfolder_alma, "Containerfile_alma")
 tempfile_arch = os.path.join(tempfolder_arch, "Containerfile_arch")
 tempfile_fedora = os.path.join(tempfolder_fedora, "Containerfile_fedora")
 tempfile_ubuntu = os.path.join(tempfolder_ubuntu, "Containerfile_ubuntu")
+if args.distro == "all" or args.distro == "alma":
+    # Remove old folder if it exists.
+    if os.path.isdir(tempfolder_alma):
+        shutil.rmtree(tempfolder_alma)
+    # Make temp folder
+    os.makedirs(tempfolder_alma, exist_ok=True)
+    # Write containerfile
+    with open(tempfile_alma, 'w') as f:
+        f.write(containerfile_alma)
+    os.chmod(tempfile_alma, 0o666)
+    print("File written to {0}".format(tempfile_alma))
 if args.distro == "all" or args.distro == "arch":
     # Remove old folder if it exists.
     if os.path.isdir(tempfolder_arch):
@@ -213,6 +249,9 @@ if args.distro == "all" or args.distro == "ubuntu":
 
 ### Build ###
 currentpath = os.getcwd()
+if args.distro == "all" or args.distro == "alma":
+    os.chdir(tempfolder_alma)
+    subprocess.run(["podman", "build", "--pull=true", "-t", "alma-img", "-f", os.path.basename(tempfile_alma)], check=True)
 if args.distro == "all" or args.distro == "arch":
     os.chdir(tempfolder_arch)
     subprocess.run(["podman", "build", "--pull=true", "-t", "arch-img", "-f", os.path.basename(tempfile_arch)], check=True)
@@ -228,11 +267,13 @@ print("""
 The following commands can be used to create and run containers:
 
 Distrobox:
+distrobox create -n alma-ct -i alma-img ; distrobox enter alma-ct
 distrobox create -n arch-ct -i arch-img ; distrobox enter arch-ct
 distrobox create -n fedora-ct -i fedora-img ; distrobox enter fedora-ct
 distrobox create -n ubuntu-ct -i ubuntu-img ; distrobox enter ubuntu-ct
 
 Distrobox (non-shared home):
+distrobox create -n alma-ct -i alma-img -H /mnt/Storage/VMs/alma-ct-home ; distrobox enter alma-ct
 distrobox create -n arch-ct -i arch-img -H /mnt/Storage/VMs/arch-ct-home ; distrobox enter arch-ct
 distrobox create -n fedora-ct -i fedora-img -H /mnt/Storage/VMs/fedora-ct-home ; distrobox enter fedora-ct
 distrobox create -n ubuntu-ct -i ubuntu-img -H /mnt/Storage/VMs/ubuntu-ct-home ; distrobox enter ubuntu-ct
