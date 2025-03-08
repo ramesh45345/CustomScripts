65b160d13a1c6572246777a794ea06374a721cb1
ramesh45345 <6318224+ramesh45345@users.noreply.github.com>
2025-03-08 07:08:08 -0500

    Update key for wine in Ubuntu tbx

--- Mtbx.py
+++ Mtbx.py
@@ -163,7 +163,7 @@ RUN apt-get install -y locales && \
 # Wine
 RUN dpkg --add-architecture i386 && \
     mkdir -pm755 /etc/apt/keyrings && \
-    wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key && \
+    wget -O - https://dl.winehq.org/wine-builds/winehq.key | gpg --dearmor -o /etc/apt/keyrings/winehq-archive.key - && \
     wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/$(lsb_release -sc)/winehq-$(lsb_release -sc).sources && \
     apt-get update && apt install -y --install-recommends winehq-devel
 """, "ubuntu")
