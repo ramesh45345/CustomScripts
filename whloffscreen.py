a4742870afbca18267923473b9ac53eae21aa1e2
ramesh45345 <6318224+ramesh45345@users.noreply.github.com>
2025-04-22 08:23:57 -0400

    Add gnome to offscreen

--- whloffscreen.py
+++ whloffscreen.py
@@ -30,9 +30,18 @@ def is_kde():
         return True
     else:
         return False
+def is_gnome():
+    """Determine if Gnome (gnome-shell) is being used."""
+    if subprocess.run(["pgrep", "gnome-shell"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=False).returncode == 0:
+        return True
+    else:
+        return False
 def kde_dpms():
     """DPMS for Plasma desktop."""
     subprocess.run("dbus-send --session --print-reply --dest=org.kde.kglobalaccel  /component/org_kde_powerdevil org.kde.kglobalaccel.Component.invokeShortcut string:'Turn Off Screen'", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True, check=True)
+def gnome_dpms():
+    """DPMS for Gnome desktop."""
+    subprocess.run("busctl --user call org.gnome.Shell /org/gnome/ScreenSaver org.gnome.ScreenSaver SetActive b true", shell=True, check=True)
 def xset_dpms():
     """DPMS for xorg."""
     subprocess.run("xset dpms force off", shell=True, check=True)
@@ -41,6 +50,8 @@ def dpms_detect():
     system_type = ""
     if is_kde():
         system_type = "kde"
+    elif is_gnome():
+        system_type = "gnome"
     elif not is_wayland():
         system_type = "x11"
     return system_type
@@ -50,6 +61,8 @@ def dpms_execute(dpms_type: str = ""):
         dpms_type = dpms_detect()
     if dpms_type == "kde":
         kde_dpms()
+    if dpms_type == "gnome":
+        gnome_dpms()
     if dpms_type == "x11":
         xset_dpms()
 
@@ -60,7 +73,7 @@ if __name__ == '__main__':
     # Get arguments
     parser = argparse.ArgumentParser(description='Turn screen off using dpms.')
     parser.add_argument("-i", "--init", help='Initial timeout before starting dpms loop.', type=int, default=5)
-    parser.add_argument("-c", "--continuous", help='Continual timeout within dpms loop.', type=int, default=20)
+    parser.add_argument("-c", "--continuous", help='Continual timeout within dpms loop.', type=int, default=30)
     args = parser.parse_args()
 
     dpms_system_type = dpms_detect()
