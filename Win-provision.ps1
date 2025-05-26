68773696e6f86f07c115df602ef4a2492eb8240d
ramesh45345 <6318224+ramesh45345@users.noreply.github.com>
2025-05-26 17:18:13 -0400

    Re-add tablacus

--- Win-provision.ps1
+++ Win-provision.ps1
@@ -536,6 +536,28 @@ function Fcn-appxremove {
   }
 }
 
+# Tablacus Function
+function Fcn-Tablacus {
+  # Install/upgrade Tablacus
+  choco upgrade -y tablacus
+
+  # Tablacus configuration
+  $pathtotablacus = "$env:PROGRAMDATA\chocolatey\lib\tablacus\tools\"
+  if(Test-Path -Path $pathtotablacus){
+    # Set rwx everyone permissions for tablacus folder
+    $Acl = Get-ACL $pathtotablacus
+    $AccessRule= New-Object System.Security.AccessControl.FileSystemAccessRule("everyone","full","ContainerInherit,Objectinherit","none","Allow")
+    $Acl.AddAccessRule($AccessRule)
+    Set-Acl $pathtotablacus $Acl
+    # Create shortcut for tablacus
+    $WshShell = New-Object -comObject WScript.Shell
+    $Shortcut = $WshShell.CreateShortcut("$env:PUBLIC\Desktop\Tablacus.lnk")
+    $Shortcut.TargetPath = "$pathtotablacus\TE64.exe"
+    $Shortcut.WorkingDirectory = "$pathtotablacus"
+    $Shortcut.Save()
+  }
+}
+
 ### Begin Code ###
 if (-Not $isDotSourced) {
   Write-Output "Running provision script."
@@ -548,6 +570,7 @@ if (-Not $isDotSourced) {
   if ( $IsVM -eq $true ) {
     Fcn-exppatch
     Fcn-DisableDefender
+    Fcn-Tablacus
     Fcn-ssh
   }
   Fcn-DisableWinRM
