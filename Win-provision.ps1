eec8b57d30957f3b6d1f63b88405e4f68337b137
ramesh45345 <6318224+ramesh45345@users.noreply.github.com>
2025-06-08 21:03:19 -0400

    Replace chocoui with unigetui

--- Win-provision.ps1
+++ Win-provision.ps1
@@ -210,7 +210,7 @@ function Fcn-Software {
   # Workaround for windows-terminal https://github.com/mkevenaar/chocolatey-packages/issues/136
   Add-AppxPackage https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx
   # GUI Apps
-  choco upgrade -y firefox notepadplusplus tortoisegit bleachbit gsudo putty chocolateygui conemu cascadiafonts vscodium sumatrapdf winget WizTree
+  choco upgrade -y firefox notepadplusplus tortoisegit bleachbit gsudo putty wingetui conemu cascadiafonts vscodium sumatrapdf winget WizTree
   choco upgrade -y ShutUp10
   # Set firefox default browser
   $associations_xml = @"
