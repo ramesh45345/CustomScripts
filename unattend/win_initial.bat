cbe6b0a4fc57af034793bf2ed850aced4098bbce
ramesh45345 <6318224+ramesh45345@users.noreply.github.com>
2025-06-19 18:03:15 -0400

    Add winget installer

--- unattend/win_initial.bat
+++ unattend/win_initial.bat
@@ -7,3 +7,6 @@ C:\Windows\SysWOW64\cmd.exe /c powershell -Command "Set-ExecutionPolicy -Executi
 REM Install Chocolatey
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 "%ALLUSERSPROFILE%\chocolatey\bin\choco.exe" upgrade -y powershell-core
+
+REM Install Winget
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -ExecutionPolicy Bypass -NoProfile -Command "iex (iwr -useb https://gist.githubusercontent.com/sht2017/3abf2fcb0cf9524004847b25e7375049/raw/getwinget.ps1)"
