1e83fb4d09b4e5beafd0786a4966609ce8ae7804
ramesh45345 <6318224+ramesh45345@users.noreply.github.com>
2025-11-14 19:59:16 -0500

    Update at script

--- Win-at.py
+++ Win-at.py
@@ -18,27 +18,15 @@ USERNAMEVAR, USERGROUP, USERHOME = CFunc.getnormaluser()
 if CFunc.is_windows() is False:
     sys.exit("ERROR, not running under Windows. Exiting.")
 
-kms_folder = os.path.join(USERHOME, "Documents", "KMS_VL_ALL_AIO")
-kms_script = os.path.join(kms_folder, "KMS_VL_ALL_AIO.cmd")
+kms_folder = os.path.join(USERHOME, "Documents")
+kms_script = os.path.join(kms_folder, "MAS_AIO.cmd")
 
-# Clone the repo
-CFunc.gitclone("https://github.com/abbodi1406/KMS_VL_ALL_AIO", kms_folder)
+CFunc.downloadfile("https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/refs/heads/master/MAS/All-In-One-Version-KL/MAS_AIO.cmd", kms_folder)
 
 # Add exceptions for defender
 Wprovision.pwsh_run(["Add-MpPreference", "-ExclusionPath", r"$env:windir\Temp\SppExtComObjHook.dll"])
 Wprovision.pwsh_run(["Add-MpPreference", "-ExclusionPath", r"$env:LOCALAPPDATA\Temp\SppExtComObjHook.dll"])
-Wprovision.pwsh_run(["Add-MpPreference", "-ExclusionPath", r"$env:windir\AutoKMS"])
-Wprovision.pwsh_run(["Add-MpPreference", "-ExclusionPath", kms_folder])
 Wprovision.pwsh_run(["Add-MpPreference", "-ExclusionProcess", kms_script])
 
-# Set auto-act
-with open(kms_script) as f:
-    s = f.read()
-snew = ""
-for line in s.splitlines():
-    snew += re.sub(r"^set uAutoRenewal=0$", "set uAutoRenewal=1", line) + "\n"
-with open(kms_script, "w") as f:
-    f.write(snew)
-
 # Run script
-subprocess.run(kms_script, shell=True, check=True)
+subprocess.run(f"{kms_script} /K-WindowsOffice", shell=True, check=True)
