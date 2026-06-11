0c6af69eeafa531e9f6838d2beb99790513eafbb
ramesh45345 <6318224+ramesh45345@users.noreply.github.com>
2026-06-11 14:06:12 -0400

    up: Add flatpak

--- up.py
+++ up.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """Update script."""
+import argparse
 import os
 import pathlib
+import re
 import shutil
 import sys
 import subprocess
@@ -10,6 +12,19 @@ import CFunc
 
 
 ### Functions ###
+def getenvfromfile(filepath: str):
+    """ Read environment variables from a file."""
+    envre = re.compile(r'''^(.+?)\s*=\s*(?:["']*)(.+?)(?:[\s"']*)$''')
+    result = {}
+    with open(filepath) as ins:
+        for line in ins:
+            match = envre.match(line)
+            if match is not None:
+                result[match.group(1)] = match.group(2)
+    return result
+def detect_update():
+    update_list = []
+    return update_list
 def ensure_root():
     """Elevate to root if not running as root"""
     if not CFunc.is_windows() and CFunc.is_root(state_exit=False):
@@ -46,10 +61,23 @@ def update_nixos():
     subprocess.run(["nixos-rebuild", "boot", "--upgrade"], check=True)
 def update_distrobox():
     subprocess.run(["distrobox-upgrade", "--all"], check=True)
+def update_flatpak():
+    if shutil.which("flatpak"):
+        subprocess.run(["flatpak", "update", "--system", "--assumeyes"], check=True)
 
 
 ### Begin Code ###
 if __name__ == "__main__":
+
+    # Get arguments
+    parser = argparse.ArgumentParser(description='Update script.')
+    parser.add_argument("-f", "--flatpak", help='Upgrade flatpak only.', action="store_true")
+    args = parser.parse_args()
+
+    if args.flatpak:
+        update_flatpak()
+        exit(0)
+
     # Ensure the script is running as root for commands that require it.
     # The original bash uses $SUDOCMD for some commands; here we run as root globally.
     ensure_root()
@@ -76,6 +104,7 @@ if __name__ == "__main__":
         user = os.environ.get("SUDO_USER") or os.environ.get("USER") or ""
         if user and user not in nix_path:
             update_nixos()
+    update_flatpak()
 
     # Non-root commands
     if not shutil.which("topgrade"):
